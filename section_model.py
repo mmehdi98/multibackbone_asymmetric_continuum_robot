e6195cda97b8241368a220c6897b7b65392e2e3c
Mehdi <mmlari77@gmail.com>
2024-10-10 10:50:45 +0200

    Plotted the reverse momentum, Fixed an error in the equations
    The solution is incorrect because the reverse momentum is too much that causes buckling and the robot needs to be modeled from the reverse direction. However the previous version can be useful in calculating the one sided movement

--- section_model.py
+++ section_model.py
@@ -7,7 +7,13 @@ import matplotlib.pyplot as plt
 num = 7
 
 def initial_guess_gen(Ft):
-    return [0.4, 0.1, 0.4, 0.1, 0.4, 0.1, 0.4, Ft * 0.95, Ft * 0.8, Ft * 0.75, Ft * 0.7, Ft * 0.65, Ft * 0.6]
+    theta_antagonist = 0.1
+    theta_protagonist = 0.4
+    theta_guess = [theta_protagonist, theta_antagonist] * 3 + [theta_protagonist]
+    Ft_guess = [Ft*(0.5+0.5*np.exp(-0.1*i)) for i in range(num-1)]
+
+    return theta_guess+Ft_guess
+    # return [0.4, 0.1, 0.4, 0.1, 0.4, 0.1, 0.4, Ft * 0.95, Ft * 0.8, Ft * 0.75, Ft * 0.7, Ft * 0.65, Ft * 0.6]
 
 A_1 = 3e-3
 A_2 = 1.2e-3
@@ -50,7 +56,7 @@ Lr = 2e-3
 Lt = np.array([Lr + R_left[i] * np.cos(a) for i, a in enumerate(alpha)])
 Lc = np.array([Lr + R_right[i] * np.cos(b) for i, b in enumerate(betha)])
 
-E = 7.5e9
+E = 35e9
 r = 0.4e-3
 I = (np.pi * r**4) / 4
 mu = 0.1
@@ -59,6 +65,7 @@ def main():
 
     Ft_values = np.arange(1, 101, 1)
     theta_solutions = []
+    reverse_momentum = []
 
     for Ft in Ft_values:
         initial_guess = initial_guess_gen(Ft)
@@ -67,29 +74,34 @@ def main():
 
         theta_solutions.append(solution[:num]) 
 
+        reverse_momentum.append(reverse_momentum_calculator(solution[:num], solution[num:], Ft))
+
+    reverse_momentum = np.array(reverse_momentum)
+    print(reverse_momentum)
+
     theta_solutions = np.array(np.degrees(theta_solutions))
     theta_solutions_sum = np.cumsum(theta_solutions, axis=1)
 
-    fig, axs = plt.subplots(1, 3, figsize=(20, 6))
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
     for i in range(num):
-        axs[0].plot(Ft_values, theta_solutions[:, i], label=f"Theta {i+1}")
+        axs[0, 0].plot(Ft_values, theta_solutions[:, i], label=f"Theta {i+1}")
     
 
-    axs[0].set_xlabel('Ft (N)')
-    axs[0].set_ylabel('Theta (degrees)')
-    axs[0].set_title('Variation of Theta with Ft')
-    axs[0].legend()
-    axs[0].grid(True)
+    axs[0, 0].set_xlabel('Ft (N)')
+    axs[0, 0].set_ylabel('Theta (degrees)')
+    axs[0, 0].set_title('Variation of Theta with Ft')
+    axs[0, 0].legend()
+    axs[0, 0].grid(True)
 
     for i in range(num):
-        axs[1].plot(Ft_values, theta_solutions_sum[:, i], label=f"Theta {i+1}")
+        axs[0, 1].plot(Ft_values, theta_solutions_sum[:, i], label=f"Theta {i+1}")
     
-    axs[1].set_xlabel('Ft (N)')
-    axs[1].set_ylabel('Cumulative Theta (degrees)')
-    axs[1].set_title('Cumulative Sum of Theta with Ft')
-    axs[1].legend()
-    axs[1].grid(True)
+    axs[0, 1].set_xlabel('Ft (N)')
+    axs[0, 1].set_ylabel('Cumulative Theta (degrees)')
+    axs[0, 1].set_title('Cumulative Sum of Theta with Ft')
+    axs[0, 1].legend()
+    axs[0, 1].grid(True)
 
     for Ft in [20, 30, 40, 60, 80, 100]:
         initial_guess = initial_guess_gen(Ft)
@@ -97,7 +109,17 @@ def main():
         solution = fsolve(lambda vars: equations(vars, Ft), initial_guess)
 
         theta_plot = (solution[:num])
-        plot_robot(theta_plot, axs[2], Ft)
+        plot_robot(theta_plot, axs[1, 0], Ft)
+
+    for i in range(int(num/2)):
+        axs[1, 1].plot(Ft_values, reverse_momentum[:, i], label=f"M{2*i+1}")
+    
+
+    axs[1, 1].set_xlabel('Ft (N)')
+    axs[1, 1].set_ylabel('Theta (degrees)')
+    axs[1, 1].set_title('Reverse Momentum')
+    axs[1, 1].legend()
+    axs[1, 1].grid(True)
 
     plt.tight_layout()
     plt.show()
@@ -188,7 +210,7 @@ def equations(vars, Ft):
                         + mu * (At[i] - R_left[i] * np.sin(th[i]))
                     )
                 )
-                + Fr[i+1] * ((Ac[i] - Ac[i+1]) + (R_left[i+1] * th[i+1] - R_left[i] * np.sin(th[i])))
+                - Fr[i+1] * ((Ac[i] - Ac[i+1]) + (R_left[i+1] * th[i+1] - R_left[i] * np.sin(th[i])))
                 - (
                     (E * I) * np.sin(th[i])
                     * (1 / (R_left[i] * (1 - np.cos(alpha[i] - th[i]))) 
@@ -261,6 +283,26 @@ def plot_robot(theta_radians, ax, Ft):
     ax.set_yticks(np.arange(-0.1, 0.9, 0.1))
     ax.grid(True)
     ax.legend()
+
+def reverse_momentum_calculator(th, Fr, Ft):
+    sum_th = np.cumsum(th)
+    M = []
+
+    for i in range(1, num-1, 2):
+        M.append(
+            np.exp(-mu * sum_th[i]) * th[i]
+            * (
+                Ft
+                * (
+                    Lt[i] / 2
+                    + R_left[i] * (np.cos(th[i]) - np.cos(alpha[i]))
+                    + mu * (At[i] - R_left[i] * np.sin(th[i]))
+                )
+            )
+            - Fr[i] * ((Ac[i] - Ac[i+1]) + (R_left[i+1] * th[i+1] - R_left[i] * np.sin(th[i])))
+        )
+
+    return M
 
 if __name__ == "__main__":
     main()
